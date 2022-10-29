7dac8a225ece00f18109ba8bb67290feddbeea1f
Pedro Miguel da Rosa <pr006453@cfjl.com.br>
2022-10-28 21:57:13 -0300

    Adicionados arquivos base para autoload (require)

---
+++ config/constants.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * DEFINIÇÕES GLOBAIS DO PROJETO
+ */
+define('FRONTEND_TITLE', 'Bicho Novo');
+define('TIMEZONE', 'Americs/Sao_Paulo');
--- public/index.php
+++ public/index.php
@@ -1,2 +1,6 @@
 <?php
     require_once __DIR__ . '/../vendor/autoload.php';
+
+    echo date('d/m/Y H:i:s');
+    echo '<br>';
+    echo FRONTEND_TITLE;
