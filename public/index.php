ea10b1042a0eb6744fffaf469632aeec6bfebe65
Pedro Miguel da Rosa <pr006453@cfjl.com.br>
2022-11-08 20:09:54 -0300

    Exemplo métodos mágicos e classe reflexão

--- public/index.php
+++ public/index.php
@@ -1,7 +1,2 @@
 <?php
     require_once __DIR__ . '/../vendor/autoload.php';
-
-    //pesquisar o que é o modelo MVC de desenvolvimento
-    //Model são as classes de acesso a dados
-    //View que são as classes/comandos de exibição de conteúdo
-    //Controller que intermediam a comunicação entre dados e visão
