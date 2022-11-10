c78973c736d54f3f2b872d1e60f2d03ba8f22b38
Pedro Miguel da Rosa <pr006453@cfjl.com.br>
2022-11-10 20:44:42 -0300

    Criados arquivos base de layout

---
+++ src/View/Render.php
@@ -0,0 +1,7 @@
+<?php
+namespace PetShop\View;
+
+class Render
+{
+    
+}
---
+++ templates/frontend/common/bottom.php
@@ -0,0 +1,12 @@
+
+<!-- BOOTSTRAP JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <!-- FONT-AWESOME -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/js/all.min.js" 
+    integrity="sha512-naukR7I+Nk6gp7p5TMA4ycgfxaZBJ7MO5iC3Fp6ySQyKFHOGfpkSZkYVWV5R7u7cfAicxanwYQ5D1e17EfJcMA==" 
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <!-- SCRIPTS DO PROJETO -->
+    <script src="/assets/js/script.js"></script>
+</body>
+</html>
