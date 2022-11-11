e942b4e7fae7d90a59bf6407adb8780261b0d525
Pedro Miguel da Rosa <pr006453@cfjl.com.br>
2022-11-10 21:05:05 -0300

    Criada render e metodo que renderiza pages no fend

--- src/View/Render.php
+++ src/View/Render.php
@@ -1,7 +1,38 @@
 <?php
 namespace PetShop\View;
 
+use Exception;
+
 class Render
 {
-    
+    /**
+     * Método que carrega uma página com a estrutura do FrontEnd, recebe 2 parâmetros:
+     *
+     * @param string $pagina Nome do arquivo a ser impresso
+     * @param array $dados Dados a serem inseridos na página
+     * @return void
+     */
+    static public function front(string $pagina, array $dados=[])
+    {
+        //monta o caminho do local onde a página localizada está
+        $pathPagina = TFRONTEND . 'pages/' . $pagina . '.php';
+
+        if (!file_exists($pathPagina)) {
+            error_log('Página template não localizada em: '. $pathPagina);
+            throw new Exception("A página solicitada '{$pagina}' não foi localizada!");
+        }
+
+        if (empty($dados['titulo'])) {
+            $dados['titulo'] = FRONTEND_TITLE;
+        } else {
+            $dados['titulo'] = $dados['titulo'] . ' - ' . FRONTEND_TITLE;
+        }
+
+        //transforma os índices do vetor em váriaveis
+        extract($dados);
+
+        require_once TFRONTEND . 'common/top.php';
+        require_once $pathPagina;
+        require_once TFRONTEND . 'common/bottom.php';
+    }
 }
