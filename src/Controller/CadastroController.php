ece61ae04d1cbc8aabb78e27d9e2b8e10dcf5aa3
Pedro Miguel da Rosa <pr006453@cfjl.com.br>
2022-11-22 19:16:26 -0300

    Criada estrutura para cadastro de cliente

--- src/Controller/CadastroController.php
+++ src/Controller/CadastroController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace PetShop\Controller;
 
 use PetShop\Core\FrontController;
@@ -6,7 +7,7 @@ use PetShop\View\Render;
 
 class CadastroController extends FrontController
 {
-    public function cadastro() 
+    public function cadastro()
     {
         $dados = [];
         $dados['titulo'] = 'Faça seu cadastro';
@@ -19,6 +20,25 @@ class CadastroController extends FrontController
 
     private function formCadastro()
     {
-       
+        $dados = [
+            'btn_label' => 'Criar minha conta',
+            'btn_class' => 'btn btn-success mt-5',
+            'fields' => [
+                [
+                    'type' => 'radio-inline', 'class' => 'col-6', 'label' => 'Você é pessoa...', 'name' => 'tipo', 'options' => [
+                        ['label' => 'Física', 'value' => 'F'],
+                        ['label' => 'Jurídica', 'value' => 'J']
+                    ],
+                    'required' => true,
+                ],
+                ['type' => 'text', 'class' => 'col-6', 'label' => 'Documento', 'name' => 'cpfcnpj', 'required' => true],
+                ['type' => 'text', 'name' => 'nome', 'label' => 'Seu nome completo', 'required' => true],
+                ['type' => 'email', 'name' => 'email', 'label' => 'Seu melhor e-mail', 'required' => true],
+                ['type' => 'password', 'class' => 'col-6', 'name' => 'senha', 'label' => 'Crie uma senha', 'required' => true],
+                ['type' => 'password', 'class' => 'col-6', 'name' => 'senha2', 'label' => 'Confirme a senha', 'required' => true],
+            ]
+        ];
+        
+        return Render::block('form', $dados);
     }
 }
---
+++ templates/frontend/pages/cadastro.php
@@ -0,0 +1,8 @@
+<div class="container my-5">
+    <div class="row">
+        <div class="col-6 mx-auto">
+            <h1 class="text-center display-6 pb-2 border-bottom mb-4">Crie sua conta</h1>
+            <?=$formCadastro?>
+        </div>
+    </div>
+</div>
