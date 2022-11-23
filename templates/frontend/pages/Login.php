966ee9ab9c634b0d4bf9fe7e5440850347b82095
Pedro Miguel da Rosa <pr006453@cfjl.com.br>
2022-11-22 21:14:48 -0300

    Criada espaço onde a mesangem será exibida

--- templates/frontend/pages/Login.php
+++ templates/frontend/pages/Login.php
@@ -1,12 +1,19 @@
 <div class="container my-5 pg-login">
+    
+    <div class="row">
+        <div class="col-6 mx-auto">
+            <?= retornaHTMLAlertMensagemSessao() ?>
+        </div>
+    </div>
+
     <div class="row">
         <div class="col-3 ms-auto border-end">
             <h3 class="text-center display-6 pb-2 border-bottom mb-3">Faça seu Logon</h3>
             <?= $formLogin ?>
         </div>
         <div class="col-3 me-auto text-center">
-        <h3 class="display-6 pb-2 border-bottom mb-3">Crie sua Conta</h3>
-        <a href="/cadastro" class="btn btn-success mt-5 w-75 btn-conta">Criar conta</a>
+            <h3 class="display-6 pb-2 border-bottom mb-3">Crie sua Conta</h3>
+            <a href="/cadastro" class="btn btn-success mt-5 w-75 btn-conta">Criar conta</a>
         </div>
     </div>
 </div>
