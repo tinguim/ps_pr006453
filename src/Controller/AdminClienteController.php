6d1bb5835fa31c7f06c91f24c94affd85c3bf060
Pedro Miguel da Rosa <pr006453@cfjl.com.br>
2022-11-28 20:41:36 -0300

    Criado formulario e processo de manutenção de dado

--- src/Controller/AdminClienteController.php
+++ src/Controller/AdminClienteController.php
@@ -1,6 +1,7 @@
 <?php
 namespace PetShop\Controller;
 
+use Exception;
 use PetShop\Model\Cliente;
 use PetShop\View\Render;
 
@@ -26,5 +27,81 @@ class AdminClienteController
         $dados['tabela'] = $htmlTabela;
 
         Render::back('clientes', $dados);
+    }
+
+    public function form($valor)
+    {
+        //verificar se o parâmetro tem um número, se for número, é um ID válido
+        if (is_numeric($valor)) {
+            $objeto = new Cliente;
+            $resultado = $objeto->find(['idcliente =' => $valor]);
+            if (empty($resultado)) {
+                redireciona('/admin/clientes', 'danger', 'Link inválido, registro não localizado!');
+            }
+            $_POST = $resultado[0];
+            $_POST['senha'] = '';
+        }
+
+        //cria e exibe o formulario
+        $dados = [];
+        $dados['titulo'] = 'Clientes - Manutenção';
+        $dados['formulario'] = $this->renderizaFormulario(empty($_POST));
+
+        Render::back('clientes', $dados);
+    }
+
+    public function postForm($valor)
+    {
+        $objeto = new Cliente;
+
+        //se $valor tem um número, carrega os dados do registro informado nele
+        if (is_numeric($valor)) {
+            if (!$objeto->loadById($valor)) {
+                redireciona('/admin/clientes', 'danger', 'Link inválido, registro não localizado!');
+            }
+        }
+
+        try {
+            $campos = array_change_key_case($objeto->getFields());
+            foreach($campos as $campo => $propriedades) {
+                if (isset($_POST[$campo])) {
+                    $objeto->$campo = $_POST[$campo];
+                }
+            }
+            $objeto->save();
+
+        } catch(Exception $e) {
+            $_SESSION['mensagem'] = [
+                'tipo' => 'warning',
+                'texto' => $e->getMessage()
+            ];
+            $this->form($valor);
+            exit;
+        }
+
+        redireciona('/admin/clientes', 'success', 'Alterações realizadas com sucesso!');
+    }
+
+    public function renderizaFormulario($novo)
+    {
+        $dados = [
+            'btn_class' => 'btn btn-warning px-5 mt-3',
+            'btn_label' => ($novo ? 'Adicionar' : 'Atualizar'),
+            'fields' => [
+                ['type'=>'readonly', 'name'=>'idcliente', 'class'=>'col-2', 'label'=>'Id. Cliente'],
+                ['type'=>'radio-inline', 'name'=>'tipo', 'class'=>'col-3', 'label'=>'Pessoa...', 'options'=>[
+                    ['value'=>'F', 'label'=>'Física'],
+                    ['value'=>'J', 'label'=>'Jurídica'],
+                ]],
+                ['type'=>'text', 'name'=>'cpfcnpj', 'class'=>'col-3', 'label'=>'Documento'],
+                ['type'=>'text', 'name'=>'nome', 'class'=>'col-4', 'label'=>'Nome Completo'],
+                ['type'=>'email', 'name'=>'email', 'class'=>'col-3', 'label'=>'E-mail'],
+                ['type'=>'password', 'name'=>'senha', 'class'=>'col-3', 'label'=>'Senha'],
+                ['type'=>'readonly', 'name'=>'created_at', 'class'=>'col-3', 'label'=>'Criado em:'],
+                ['type'=>'readonly', 'name'=>'updated_at', 'class'=>'col-3', 'label'=>'Atualizado em:'],
+            ]
+        ];
+
+        return Render::block('form', $dados);
     }
 }
