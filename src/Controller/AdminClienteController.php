1efaac438e24fb573d20f33f76e2cd166f130f18
Pedro Miguel da Rosa <pr006453@cfjl.com.br>
2022-11-28 17:06:21 -0300

    usando bloco que gera listagem de dados

--- src/Controller/AdminClienteController.php
+++ src/Controller/AdminClienteController.php
@@ -1,15 +1,29 @@
 <?php
 namespace PetShop\Controller;
 
-use PetShop\Core\FrontController;
+use PetShop\Model\Cliente;
 use PetShop\View\Render;
 
-class AdminClienteController extends FrontController
+class AdminClienteController
 {
     public function listar()
     {
+        //Alimentando dados para a tabela de listagem
+        $dadosListagem = [];
+        $dadosListagem['objeto'] = new Cliente;
+        $dadosListagem['colunas'] = [
+            ['campo'=>'idcliente', 'class'=>'text-center'],
+            ['campo'=>'tipo', 'class'=>'text-center'],
+            ['campo'=>'nome'],
+            ['campo'=>'email'],
+            ['campo'=>'created_at', 'class'=>'text-center'],
+        ];
+        $htmlTabela = Render::block('tabela-admin', $dadosListagem);
+
+        //Alimentando dados para a página de clientes
         $dados = [];
         $dados['titulo'] = 'Clientes';
+        $dados['tabela'] = $htmlTabela;
 
         Render::back('clientes', $dados);
     }
