1092119f24182bbeb390e3d022e602e1fe9d8e89
Pedro Miguel da Rosa <pr006453@cfjl.com.br>
2023-02-17 11:12:01 -0300

    ajustes

--- src/Controller/PromocoesController.php
+++ src/Controller/PromocoesController.php
@@ -2,11 +2,9 @@
 
 namespace PetShop\Controller;
 
-use PetShop\Core\Exception;
 use PetShop\Core\FrontController;
-use PetShop\Core\SendMail;
+use PetShop\Model\Promocoes;
 use PetShop\View\Render;
-use Respect\Validation\Validator as v;
 
 class PromocoesController extends FrontController
 {
@@ -16,56 +14,16 @@ class PromocoesController extends FrontController
         $dados = [];
         $dados['topo'] = $this->carregaHTMLTopo();
         $dados['rodape'] = $this->carregaHTMLRodape();
-        $dados['formulario'] = $this->formPromocoes();
+        //$dados['empresa'] = $_SESSION['empresa'];
+
+        $promocoes = (new Promocoes)->find();
+        foreach ($promocoes as &$e) {
+            $promoAtual = new Promocoes;
+            $promoAtual->loadById($e['idpromocao']);
+            //$e['imagens'] = $promoAtual->getFiles();
+        }
+        $dados['promocoes'] = $promocoes;
 
         Render::front('promocoes', $dados);
-    }
-
-    public function postPromocoes()
-    {
-        try {
-            if (empty($_POST['titulo']) || empty($_POST['descricao']) || empty($_POST['mensagem'])) {
-                throw new Exception('Todos os campos devem ser preenchidos');
-            }
-            $titulo = trim($_POST['titulo']);
-            $descricao = trim($_POST['descricao']);
-            if (strlen($titulo)<6) {
-                throw new Exception('O nome precisa ser completo');
-            }
-            if (strlen($descricao)<6) {
-                throw new Exception('Por favor, seja mais descritivo na mensagem');
-            }
-            $assunto = 'Contato via site - ' . date('d/m/Y H:i:s');
-            $mensagemFull = <<<HTML
-                Olá, chegou uma nova promoção<br>
-                <strong>Título:</strong> {$titulo}<br>
-                <strong>Descrição:</strong> <br>{$descricao}<br>
-            HTML;
-
-            SendMail::enviar(MAIL_CONTACTNAME, MAIL_CONTACTMAIL, $assunto, $mensagemFull, $titulo);
-
-        } catch(Exception $e) {
-            $_SESSION['mensagem'] = [
-                'tipo'=>'warning',
-                'texto'=>$e->getMessage()
-            ];
-            $this->promocoes();
-            exit;
-        }
-        redireciona('/promocoes', 'success', 'Mensagem enviada com sucesso');
-    }
-
-    private function formPromocoes()
-    {
-        $dados = [
-            'btn_label' => 'Enviar mensagem',
-            'btn_class' => 'btn btn-warning w-50',
-            'fields' => [
-                ['type' => 'text', 'name' => 'titulo', 'label' => 'Título', 'required' => true],
-                ['type' => 'textarea', 'name' => 'descricao', 'label' => 'Descrição', 'rows' => 5, 'required' => true],
-            ]
-        ];
-
-        return Render::block('form', $dados);
     }
 }
--- templates/frontend/pages/promocoes.php
+++ templates/frontend/pages/promocoes.php
@@ -1,8 +1,13 @@
+<?php
+
+use PetShop\Model\Promocoes;
+?>
 <div class="container my-5">
     <div class="row">
         <div class="col-6 mx-auto">
             <h1 class="text-center display-6 pb-2 border-bottom mb-4">Promoções</h1>
             <?=retornaHTMLAlertMensagemSessao()?>
+            <?=$dados['promocoes'][0]?>
         </div>
     </div>
 </div>
