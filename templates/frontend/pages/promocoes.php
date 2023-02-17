c27b0295d7adbfd8923e3bfab034acf53dd53e5d
Pedro Miguel da Rosa <pr006453@cfjl.com.br>
2023-02-17 11:12:28 -0300

    ajuste

--- templates/frontend/pages/promocoes.php
+++ templates/frontend/pages/promocoes.php
@@ -7,7 +7,6 @@ use PetShop\Model\Promocoes;
         <div class="col-6 mx-auto">
             <h1 class="text-center display-6 pb-2 border-bottom mb-4">Promoções</h1>
             <?=retornaHTMLAlertMensagemSessao()?>
-            <?=$dados['promocoes'][0]?>
         </div>
     </div>
 </div>
