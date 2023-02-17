a012e91454edecfebc4d5b24cd394fd205d6c180
Pedro Miguel da Rosa <pr006453@cfjl.com.br>
2023-02-17 10:54:42 -0300

    ajustes

--- src/Controller/PromocoesController.php
+++ src/Controller/PromocoesController.php
@@ -8,7 +8,7 @@ use PetShop\Core\SendMail;
 use PetShop\View\Render;
 use Respect\Validation\Validator as v;
 
-class FaleConoscoController extends FrontController
+class PromocoesController extends FrontController
 {
     public function promocoes()
     {
@@ -16,7 +16,7 @@ class FaleConoscoController extends FrontController
         $dados = [];
         $dados['topo'] = $this->carregaHTMLTopo();
         $dados['rodape'] = $this->carregaHTMLRodape();
-        $dados['formulario'] = $this->formFaleConosco();
+        $dados['formulario'] = $this->formPromocoes();
 
         Render::front('promocoes', $dados);
     }
@@ -24,52 +24,45 @@ class FaleConoscoController extends FrontController
     public function postPromocoes()
     {
         try {
-            if (empty($_POST['nome']) || empty($_POST['email']) || empty($_POST['mensagem'])) {
+            if (empty($_POST['titulo']) || empty($_POST['descricao']) || empty($_POST['mensagem'])) {
                 throw new Exception('Todos os campos devem ser preenchidos');
             }
-            $nome = trim($_POST['nome']);
-            $email = trim($_POST['email']);
-            $mensagem = trim($_POST['mensagem']);
-            if (strlen($nome)<6) {
+            $titulo = trim($_POST['titulo']);
+            $descricao = trim($_POST['descricao']);
+            if (strlen($titulo)<6) {
                 throw new Exception('O nome precisa ser completo');
             }
-            $emailValido = v::email()->validate($email);
-            if (!$emailValido) {
-                throw new Exception('O e-mail está incorreto');
-            }
-            if (strlen($mensagem)<6) {
+            if (strlen($descricao)<6) {
                 throw new Exception('Por favor, seja mais descritivo na mensagem');
             }
             $assunto = 'Contato via site - ' . date('d/m/Y H:i:s');
             $mensagemFull = <<<HTML
-                Olá, chegou um novo contato<br>
-                <strong>Nome:</strong> {$nome}<br>
-                <strong>E-mail:</strong> {$email}<br>
-                <strong>Mensagem:</strong> <br>{$mensagem}<br>
+                Olá, chegou uma nova promoção<br>
+                <strong>Título:</strong> {$titulo}<br>
+                <strong>Descrição:</strong> <br>{$descricao}<br>
             HTML;
 
-            SendMail::enviar(MAIL_CONTACTNAME, MAIL_CONTACTMAIL, $assunto, $mensagemFull, $nome, $email);
+            SendMail::enviar(MAIL_CONTACTNAME, MAIL_CONTACTMAIL, $assunto, $mensagemFull, $titulo);
 
         } catch(Exception $e) {
             $_SESSION['mensagem'] = [
                 'tipo'=>'warning',
                 'texto'=>$e->getMessage()
             ];
-            $this->faleConosco();
+            $this->promocoes();
             exit;
         }
-        redireciona('/fale-conosco', 'success', 'Mensagem enviada com sucesso');
+        redireciona('/promocoes', 'success', 'Mensagem enviada com sucesso');
     }
 
-    private function formFaleConosco()
+    private function formPromocoes()
     {
         $dados = [
             'btn_label' => 'Enviar mensagem',
             'btn_class' => 'btn btn-warning w-50',
             'fields' => [
-                ['type' => 'text', 'name' => 'nome', 'label' => 'Nome completo', 'required' => true],
-                ['type' => 'email', 'name' => 'email', 'label' => 'E-mail', 'required' => true],
-                ['type' => 'textarea', 'name' => 'mensagem', 'label' => 'Mensagem', 'rows' => 5, 'required' => true],
+                ['type' => 'text', 'name' => 'titulo', 'label' => 'Título', 'required' => true],
+                ['type' => 'textarea', 'name' => 'descricao', 'label' => 'Descrição', 'rows' => 5, 'required' => true],
             ]
         ];
 
--- templates/frontend/pages/promocoes.php
+++ templates/frontend/pages/promocoes.php
@@ -1,34 +1,8 @@
-<div class="container">
-
-    <div class="row w-75 mx-auto">
-        <div class="col-12 text-center mt-1 mb-3">
-            <h1><?=$promocoes['nome']?></h1>
+<div class="container my-5">
+    <div class="row">
+        <div class="col-6 mx-auto">
+            <h1 class="text-center display-6 pb-2 border-bottom mb-4">Promoções</h1>
+            <?=retornaHTMLAlertMensagemSessao()?>
         </div>
-        <div class="col-2">
-            <img src="<?=$promocoes['imagens'][0]['url']?>" class="img-fluid" alt="<?=$promocoes['nome']?>">
-        </div>
-        <div class="col-10">
-            <?=$promocoes['descricao']?></h1>
-        </div>
-    </div>
-
-    <hr class="mb-5">
-
-    <div class="row row-cols-5">
-        <?php
-            foreach($promocoes as $p) {
-                $primeiraImagem = $p['imagens'][0]['url'] ?? 'https://picsum.photos/300';
-                $preco = number_format($p['preco'], 2, ',', '.');
-                $nome = strlen($p['nome'])<50 ? $p['nome'] : substr($p['nome'], 0, 50) . '...';
-                $link = '/promocoes/' . $p['idpromocao'];
-                echo <<<HTML
-                    <div clas="col produto position-relative mb-3">
-                        <img src="{$primeiraImagem}" class="img-fluid" alt="{$p['nome']}">
-                        <p class="mb-0 mt-2">{$nome}</p>
-                        <a class="btn btn-success btn-sm w-100 my-2 stretched-link" href="{$link}">R$ {$preco}</a>
-                    </div>
-                HTML;
-            }
-        ?>
     </div>
 </div>
