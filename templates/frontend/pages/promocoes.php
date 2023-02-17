7ef4928b3271df0da37b86bd1524559e3f183371
Pedro Miguel da Rosa <pr006453@cfjl.com.br>
2023-02-17 11:50:39 -0300

    final

--- templates/frontend/pages/promocoes.php
+++ templates/frontend/pages/promocoes.php
@@ -5,14 +5,8 @@
             <h1 class="text-center display-6 pb-2 border-bottom mb-4">Promoções</h1>
             <?=retornaHTMLAlertMensagemSessao()?>
             <?php foreach($promocoes as $p): ?>
-            <div class="col-2">
-                <img src="<?=$p['imagens'][0]['url']?>" alt="" class="img-fluid">
-            </div>
             <div class="col-6">
                 <?=$p['titulo']?>
-            </div>
-            <div class="col-2">
-                <input type="text" value="<?=$p['descricao']?>">
             </div>
         </div>
     </div>
